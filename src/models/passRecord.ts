66c59e1d156bb7217c782b1e4a63038b78cf40a2
OG-Gardavsky <ogardavsky@gmail.com>
2022-10-04 23:28:53 +0200

    basic endpoint for saving records

--- src/models/passRecord.ts
+++ src/models/passRecord.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 
-interface IPassRecord {
+export interface IPassRecord {
     name: string;
     userName: string;
     password: string;
--- src/routes/passRecord.ts
+++ src/routes/passRecord.ts
@@ -1,11 +1,49 @@
 import { Router, Request, Response } from 'express';
-
+import { IPassRecord, PassRecord } from "../models/passRecord";
+import { HydratedDocument } from 'mongoose';
 
 const router: Router = Router();
 
+router.post('/', async (req: Request, res: Response) => {
+
+    const passRecord: HydratedDocument<IPassRecord>  = new PassRecord({
+        ...req.body
+    })
+
+    //TODO fill user from auth token
+    const user: string = 'John doe';
+
+    try {
+        await passRecord.validate();
+    } catch (err) {
+
+        console.error(`PassRecord for user ${user} was not save due to validation errors`);
+        return res.status(400).send(err);
+    }
+
+
+    try {
+        await passRecord.save();
+        console.log(`User: ${user} added new record with name: ${passRecord.name}`)
+
+        res.send(passRecord);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({error: 'Unexpected server error'});
+    }
+});
+
 router.get('/', async (req: Request, res: Response) => {
 
-    res.send('works')
 });
+
+router.put('/', async (req: Request, res: Response) => {
+
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+
+});
+
 
 export { router };
