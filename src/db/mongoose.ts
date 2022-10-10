c58696607239c850ddc5db369e14ed86b6840d84
OG-Gardavsky <ogardavsky@gmail.com>
2022-10-10 21:26:31 +0200

    modified db connection

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,10 +1,12 @@
-import { Mongoose } from 'mongoose';
-import mongoose from 'mongoose';
+import { connect } from 'mongoose';
 import { generalConfig } from '../config/generalConfig';
 
-export const dbConnect = () => {
-    mongoose.connect(String(generalConfig.mongoDbAddress))
-        .then((mongoose: Mongoose) => console.log(`connected to db: '${generalConfig.mongoDbAddress}'`))
-        .catch((err) => console.error(generalConfig.mongoDbAddress, err))
-}
 
+export const dbConnect = async () => {
+    try {
+        await connect(String(generalConfig.mongoDbAddress))
+        console.log(`connected to db: '${generalConfig.mongoDbAddress}'`);
+    } catch (err) {
+        console.error(generalConfig.mongoDbAddress, err);
+    }
+}
