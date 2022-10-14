520bd3e0244bbc13957d2c70f7fe1518a294375a
OG-Gardavsky <ogardavsky@gmail.com>
2022-10-14 11:31:27 +0200

    passport try-out

--- src/config/generalConfig.ts
+++ src/config/generalConfig.ts
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const generalConfig = {
     mongoDbAddress: process.env.MONGO_DB_ADDRESS,
-
+    googleAuthclientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    googleAuthclientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
 }
